770a6dbcb630b0e9c6de211dbc7108bf5d0b2153
Samith789 <hsedepartmenttabnih@gmail.com>
2025-12-30 14:11:56 +0300

    Add HTML form for file conversion API

---
+++ api.js
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head><title>Convert API snippet</title></head>
+    <body>
+        <form action="https://v2.convertapi.com/convert/pdf/to/docx?auth=3h8UBiRtvHf3F4LolOrJxc9BHuyPN54O&download=attachment" method="post" enctype="multipart/form-data">
+            <input type="file" name="File" />
+            <input type="submit" value="Convert file"/>
+        </form>
+    </body>
+</html>
